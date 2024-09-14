7d0180af8f34962a9d6765f3a5fcf0b79cd8b394
thameemansari <thameemrpp@gmail.com>
2024-09-14 12:54:40 +0530

    Initial commit

---
+++ PF-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <div>
+    <h1 className='text-3xl text-teal-950'>Assalamu alakkum </h1>
+  </div>
+  )
+}
+
+export default App
